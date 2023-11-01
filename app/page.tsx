6f298d3282769bc598d2c999bae3ade6b16610da
Towsif Hossain <towsifhossain5120@gmail.com>
2023-11-01 18:26:58 +0600

    images added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
 import Image from 'next/image'
 
 export default function Home() {
+  type ImageURL = {
+    name: string;
+    key: string;
+  }
+  const images_url: ImageURL[] = [
+    {name: 'image-1.webp', key: '1'},
+    {name: 'image-2.webp', key: '2'},
+    {name: 'image-3.webp', key: '3'},
+    {name: 'image-4.webp', key: '4'},
+    {name: 'image-5.webp', key: '5'},
+    {name: 'image-6.webp', key: '6'},
+    {name: 'image-7.webp', key: '7'},
+    {name: 'image-8.webp', key: '8'},
+    {name: 'image-9.webp', key: '9'},
+    {name: 'image-10.jpeg', key: '10'},
+    {name: 'image-11.jpeg', key: '11'},
+    
+  ];
   return (
     <div className='h-screen bg-gradient-to-br from-blue-900 via-blue-700 to-blue-200 flex flex-col'>
       <div className='flex w-screen justify-center'>
@@ -12,19 +30,19 @@ export default function Home() {
       <div className='flex w-screen justify-center'>
         <button className='m-3 p-3 rounded-md bg-white'> Delete </button>
       </div>
-      <div className='img-grid'>
-        <div className='featured-card'>1</div>
-        <div className='img-card'>2</div>
-        <div className='img-card'>3</div>
-        <div className='img-card'>4</div>
-        <div className='img-card'>5</div>
-        <div className='img-card'>6</div>
-        <div className='img-card'>7</div>
-        <div className='img-card'>8</div>
-        <div className='img-card'>9</div>
-        <div className='img-card'>10</div>
-        <div className='img-card'>11</div>
-        <div className='img-card'>12</div>
+      <div className='flex justify-center'>
+        <div className='img-grid'>
+          <div className='featured-card'> <img src='/images/image-1.webp'/> </div>
+          {
+            images_url.map((image_) => (
+              (
+                <div key={image_.key} className='img-card'>
+                  <img src={'/images/'+image_.name} alt={image_.name} />
+                </div>
+              )
+            ))
+          } 
+        </div>
       </div>
     </div>
   )
