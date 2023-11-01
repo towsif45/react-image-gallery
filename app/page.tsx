c6a699b2b33e48dba4c2423429e2b97eca346a9c
Towsif Hossain <towsifhossain5120@gmail.com>
2023-11-01 17:14:43 +0600

    page background improved

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='h-screen bg-slate-400 flex flex-col'>
-      <div className='flex w-screen justify-center bg-yellow-400'>
-        <h1 className='m-5 text-7xl font-bold h-min bg-white'> Gallery </h1>
+    <div className='h-screen bg-gradient-to-br from-blue-900 via-blue-700 to-blue-200 flex flex-col'>
+      <div className='flex w-screen justify-center'>
+        <h1 className='m-5 p-3 rounded-lg text-7xl font-bold h-min bg-white'> Gallery </h1>
       </div>
-      <div className='flex w-screen justify-center bg-yellow-400'>
+      <div className='flex w-screen justify-center'>
         <h1 className='m-5 bg-white'> Selected x items </h1>
       </div>
-      <div className='flex w-screen justify-center bg-yellow-400'>
-        <button className='m-3 bg-white'> Delete </button>
+      <div className='flex w-screen justify-center'>
+        <button className='m-3 p-3 rounded-md bg-white'> Delete </button>
       </div>
-      <div className='grid mt-3 ml-24 mr-24 px-4 py-4 bg-white sm:grid-cols-5 gap-4'>
-        <div className='row-span-2 col-span-2 rounded-lg h-full w-full bg-red-400'>1</div>
+      <div className='img-grid'>
+        <div className='featured-card'>1</div>
         <div className='img-card'>2</div>
         <div className='img-card'>3</div>
         <div className='img-card'>4</div>
