f35fbe27cd5dd34062fc21703d692a528a8fe426
Towsif Hossain <towsifhossain5120@gmail.com>
2023-11-02 18:31:06 +0600

    animations improved

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,8 @@
 import Image from 'next/image'
-
-type ImageURL = {
-  name: string;
-  key: string;
-}
+import Grid from './components/grid'
 
 export default function Home() {
-  const images_url: ImageURL[] = [
-    {name: 'image-1.webp', key: '1'},
-    {name: 'image-2.webp', key: '2'},
-    {name: 'image-3.webp', key: '3'},
-    {name: 'image-4.webp', key: '4'},
-    {name: 'image-5.webp', key: '5'},
-    {name: 'image-6.webp', key: '6'},
-    {name: 'image-7.webp', key: '7'},
-    {name: 'image-8.webp', key: '8'},
-    {name: 'image-9.webp', key: '9'},
-    {name: 'image-10.jpeg', key: '10'},
-    {name: 'image-11.jpeg', key: '11'}, 
-  ];
+  
   return (
     <div className='body'>
       <div className='flex w-screen justify-center'>
@@ -31,38 +15,7 @@ export default function Home() {
         <button className='m-3 p-3 rounded-md bg-white'> Delete </button>
       </div>
       <div className='flex justify-center'>
-        <div className='img-grid'>
-        {
-          images_url.map((element, id) => {
-            if(id === 0){
-              return(
-                <div key={element.key} className='featured-card group hover:scale-105 duration-300'> 
-                  <input type='checkbox' className='checkbox peer z-10' />
-                  <Image 
-                    src={'/images/'+element.name} 
-                    alt={element.name} 
-                    width="0"
-                    height="0"
-                    sizes="100vw"
-                    className='rounded-lg peer-checked:opacity-50 z-0 w-full h-auto'/> 
-                </div>
-              )
-            }
-            return (
-              <div key={element.key} className='img-card group hover:scale-105 duration-300'>
-                  <input type='checkbox' className='checkbox peer z-10' />
-                  <Image 
-                    src={'/images/'+element.name} 
-                    alt={element.name} 
-                    width="0"
-                    height="0"
-                    sizes="100vw"
-                    className='rounded-lg peer-checked:opacity-50 z-0 w-full h-auto' />
-              </div>
-            )
-          })
-        } 
-        </div>
+        <Grid />
       </div>
     </div>
   )
