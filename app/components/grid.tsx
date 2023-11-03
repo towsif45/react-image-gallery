967c0951f61c5062f973b9e67a1c21e9ec95625b
Towsif Hossain <towsifhossain5120@gmail.com>
2023-11-03 17:32:48 +0600

    loading images using useEffect

--- app/components/grid.tsx
+++ app/components/grid.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image';
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
 type ImageURL = {
     name: string;
@@ -9,23 +9,31 @@ type ImageURL = {
   }
 
 const Grid = () => {
-    const images_url: ImageURL[] = [
-        {name: 'image-1.webp', key: '1'},
-        {name: 'image-2.webp', key: '2'},
-        {name: 'image-3.webp', key: '3'},
-        {name: 'image-4.webp', key: '4'},
-        {name: 'image-5.webp', key: '5'},
-        {name: 'image-6.webp', key: '6'},
-        {name: 'image-7.webp', key: '7'},
-        {name: 'image-8.webp', key: '8'},
-        {name: 'image-9.webp', key: '9'},
-        {name: 'image-10.jpeg', key: '10'},
-        {name: 'image-11.jpeg', key: '11'}, 
-    ];
+    const [data, setData] = useState<ImageURL[]>([]);
+    
+    const images_url: ImageURL[] = useMemo(() => 
+        [
+            {name: 'image-1.webp', key: '1'},
+            {name: 'image-2.webp', key: '2'},
+            {name: 'image-3.webp', key: '3'},
+            {name: 'image-4.webp', key: '4'},
+            {name: 'image-5.webp', key: '5'},
+            {name: 'image-6.webp', key: '6'},
+            {name: 'image-7.webp', key: '7'},
+            {name: 'image-8.webp', key: '8'},
+            {name: 'image-9.webp', key: '9'},
+            {name: 'image-10.jpeg', key: '10'},
+            {name: 'image-11.jpeg', key: '11'}, 
+        ], []
+    ) ;
+
+    useEffect(()=>{
+        setData(images_url);
+    }, [images_url])
     return (
         <div className='img-grid'>
         {
-          images_url.map((element, id) => {
+          data.map((element, id) => {
             if(id === 0){
               return(
                 <div key={element.key} className='featured-card group hover:scale-105 duration-300'> 
